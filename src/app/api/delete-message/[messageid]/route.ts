b9ea743c3db0eb2375f44ec6abcab67417b00616
Koustavjr <Koustavjr11@gmail.com>
2024-08-27 19:13:57 +0530

    delete route already deleted

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+import {  User } from "next-auth";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function DELETE(request:Request,
+    {params}:{params:{messageid:string}}
+)
+{   
+    const messageId= params.messageid;
+    await dbConnect();
+    const session= await getServerSession(authOptions);
+    const user:User = session?.user;
+
+    if(!session || session.user)
+    {
+        return Response.json({
+            success:false,
+            message:"Error in deleting message"
+        },
+    {status:400})
+    }
+
+    try {
+        const updateResult = await UserModel.updateOne(
+            {_id:user._id},
+            {$pull:{messages:{_id:messageId}}}
+        )        
+
+        if(updateResult.modifiedCount===0)
+        {
+            return Response.json({
+                success:false,
+                message:"Message already deleted"
+            },
+        {status:404})
+        }
+
+        return Response.json({
+            success:true,
+            message:"Message already deleted"
+        },
+    {status:200})
+    } catch (error) {
+        return Response.json({
+            success:false,
+            message:"Error in deleting message"
+        },
+    {status:500})
+    }
+
+}
