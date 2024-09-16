e112c0938b857d29962d8ad78397844106be5ca8
Koustavjr <Koustavjr11@gmail.com>
2024-09-16 15:43:10 +0530

    project completed

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -22,8 +22,8 @@ export async function POST(req: Request) {
 
   // return result.toDataStreamResponse();
   try {
-    //const prompt=  "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
-    const prompt= "tell me about  turkey"
+    const prompt=  "Create a list of three open-ended and engaging questions without mentioning here are three open-ended and engaging, unique each time questions on output formatted as a single string. Each question should be separated by '||' and be unique each  time. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment ";
+    //const prompt= "tell me about  turkey"
   const result = await streamText({
     model: groq('llama3-8b-8192'),
     system: 'You are a helpful assistant.',
